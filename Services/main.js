032bbf4525c7423a303c111e6f9fe8e7052322cd
Franck Rochat <fun13@live.fr>
2020-12-21 00:30:00 +0100

    Update main.js

--- Services/main.js
+++ Services/main.js
@@ -1,86 +1,70 @@
-/*
-____                    _        _
-|  _ \                  | |      (_)
-| |_) | __ _ _ __   __ _| | ___   _ ___
-|  _ < / _` | '_ \ / _` | |/ _ \ | / __|
-| |_) | (_| | | | | (_| | |  __/_| \__ \
-|____/ \__,_|_| |_|\__, |_|\___(_) |___/
-                   __/ |       _/ |
-                  |___/       |__/
+//AIDE: https://www.espruino.com/Bangle.js#-a-name-lcd-a-lcd-screen
+//AIDE: https://banglejs.com/reference#l_Bangle_GPS
+//AIDE: https://www.espruino.com/Bangle.js+Development
+//---------------------------------------------------------------------------
+//Emplacement des variables
+//---------------------------------------------------------------------------
 
-Hello! This is the Bangle.js emulator. It's a special
-version of espruino.com/ide that runs a version of
-the Espruino JavaScript interpreter in your browser.
+//---------------------------------------------------------------------------
+//Emplacement des blibliotheques
+//---------------------------------------------------------------------------
 
-Want a real Bangle.js? Check out https://shop.espruino.com/banglejs
+g.clear(); //Méthode permettant de nettoyer l'ecran
+print("Ecran nettoyer"); //Fonction permettant d'écrire dans la console.
+//E.showMessage("Hello","A Title"); //Une façon d'écrire un texte avec/sans un titre
+Terminal.println("Code Name: ''Avonistan'' "); //Une autre façon d'écrire un texte sans mise-en-page ni titre; Ecriture simple.
 
-Want to try some applications? Head to https://banglejs.com/apps/
-and click on any applications that have the emulator icon next to them.
+setWatch(() => {
+  g.clear(); //Méthode permettant de nettoyer l'ecran
+  print("Bouton N°1"); //Fonction permettant d'écrire dans la console.
+  Bangle.setGPSPower(0);     //On eteint le GPS
+  Bangle.setCompassPower(0); //On eteint la boussole
+  Terminal.println("Bouton N°1");
+}, BTN1, {repeat:true});
 
-For information on getting started, check out:
+setWatch(() => { //Si le bouton du millieux (Bouton 2) est actionnee alors, le GPS est activé puis la montre affiche les infos GPS.
+  g.clear(); //Méthode permettant de nettoyer l'ecran
 
-* https://www.espruino.com/Bangle.js#-a-name-lcd-a-lcd-screen
-* https://banglejs.com/reference
+  print("Bouton N°2"); //Fonction permettant d'écrire dans la console.
+  //-------------------------GPS--------------------------
+  Bangle.setCompassPower(0); //On eteint la boussole
+  Bangle.setGPSPower(1);     //On allume le GPS
+  Bangle.on('GPS', function(gps){
 
-To run the app below, just click the upload button to the left
-of this editor window!
-*/
+    Terminal.print("Lat: "+ gps.lat);
+    Terminal.println(" - Long: "+gps.lon);
+    Terminal.println("Vitesse: "+gps.speed);
+    Terminal.println("Altitude: "+gps.alt + " M");
+    Terminal.println("Satellites: "+gps.satellites);
+    Terminal.println(gps.time);
+  });
+  //-------------------------GPS--------------------------
+  Terminal.println("Bouton N°2");
+  //setTimeout(()=>g.clear(), 1000);
+}, BTN2, {repeat:true});
 
-// Draw a rotating cube wireframe...
-// X and Y rotation
-var rx = 0, ry = 0;
 
-// draw a cube
-function draw() {
-  var rcx=Math.cos(rx),
-      rsx=Math.sin(rx),
-      rcy=Math.cos(ry),
-      rsy=Math.sin(ry);
-  // Project 3D coordinates into 2D
-  function p(x,y,z) {
-    var t;
-    t = x*rcy + z*rsy;
-    z = z*rcy - x*rsy;
-    x=t;
-    t = y*rcx + z*rsx;
-    z = z*rcx - y*rsx;
-    y=t;
-    z += 4;
-    return [g.getWidth() * (0.5 + x/z), g.getHeight() * (0.5 + y/z)];
+setWatch(() => {
+  g.clear(); //Méthode permettant de nettoyer l'ecran
+  print("Bouton N°3"); //Fonction permettant d'écrire dans la console.
+  //-------------------------Boussole--------------------------
+  Bangle.setGPSPower(0);     //On eteint le GPS
+  Bangle.setCompassPower(1); //On allume la boussole
+  Bangle.on('mag', function(compass){
+
+  if (isNaN(compass.heading)) {
+    Terminal.println("Boussole non-calibré,");
+    Terminal.println("Tournez sur vous-même à 360°.");
+    print("Boussole non-calibré.");
+    //Bangle.buzz();
   }
 
-  var a;
-  // draw a series of lines to make up our cube
-  a = p(-1,-1,-1); g.moveTo(a[0],a[1]);
-  a = p(1,-1,-1); g.lineTo(a[0],a[1]);
-  a = p(1,1,-1); g.lineTo(a[0],a[1]);
-  a = p(-1,1,-1); g.lineTo(a[0],a[1]);
-  a = p(-1,-1,-1); g.lineTo(a[0],a[1]);
-  a = p(-1,-1,1); g.moveTo(a[0],a[1]);
-  a = p(1,-1,1); g.lineTo(a[0],a[1]);
-  a = p(1,1,1); g.lineTo(a[0],a[1]);
-  a = p(-1,1,1); g.lineTo(a[0],a[1]);
-  a = p(-1,-1,1); g.lineTo(a[0],a[1]);
-  a = p(-1,-1,-1); g.moveTo(a[0],a[1]);
-  a = p(-1,-1,1); g.lineTo(a[0],a[1]);
-  a = p(1,-1,-1); g.moveTo(a[0],a[1]);
-  a = p(1,-1,1); g.lineTo(a[0],a[1]);
-  a = p(1,1,-1); g.moveTo(a[0],a[1]);
-  a = p(1,1,1); g.lineTo(a[0],a[1]);
-  a = p(-1,1,-1); g.moveTo(a[0],a[1]);
-  a = p(-1,1,1); g.lineTo(a[0],a[1]);
-}
+    else{
+      Terminal.println(compass.heading + " °degrée");
+    }
+  //Bangle.setCompassPower(0);
+  });
+  //-------------------------Boussole--------------------------
 
-// rotate and redraw the cube
-function step() {
-  // rotate
-  rx += 0.1;
-  ry += 0.11;
-  // draw
-  g.clear();
-  g.setColor(0xFFFF);
-  draw();
-}
-
-g.clear();
-setInterval(step,50);
+  Terminal.println("Bouton N°3");
+}, BTN3, {repeat:true});
